a02b4f17b46415d1427bbfab9c5d6aba4e44f191
Jack Rugile <jackrugile@gmail.com>
2011-12-03 23:35:38 -0700

    Internet Explorer filter typo fix ("apha" changed to "alpha")

--- jquery.jrumble.js
+++ jquery.jrumble.js
@@ -1,189 +1,126 @@
 /*
-jRumble v1.1 - http://jackrugile.com/jrumble
+jRumble v1.3 - http://jackrugile.com/jrumble
 by Jack Rugile - http://jackrugile.com
-Copyright 2011, Jack Rugile
-MIT license - http://www.opensource.org/licenses/mit-license.php
+
+MIT License
+-----------------------------------------------------------------------------
+Copyright (c) 2011 Jack Rugile, http://jackrugile.com
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 (function($){
 	$.fn.jrumble = function(options){
 		
-		// JRUMBLE OPTIONS
-		//---------------------------------
+		/*========================================================*/
+		/* Options
+		/*========================================================*/
 		var defaults = {
-			rangeX: 2,
-			rangeY: 2,
-			rangeRot: 1,
-			rumbleSpeed: 10,
-			rumbleEvent: 'hover',
-			posX: 'left',
-			posY: 'top'
-		};
-
-		var opt = $.extend(defaults, options);
-
+			x: 2,
+			y: 2,
+			rotation: 1,
+			speed: 15,
+			opacity: false,
+			opacityMin: .5
+		},
+		opt = $.extend(defaults, options);	
+				
 		return this.each(function(){
+								  
+			/*========================================================*/
+			/* Variables
+			/*========================================================*/
+			var $this = $(this),				
+				x = opt.x*2,
+				y = opt.y*2,
+				rot = opt.rotation*2,
+				speed = (opt.speed === 0) ? 1 : opt.speed,			
+				opac = opt.opacity,
+				opacm = opt.opacityMin,
+				inline,
+				interval;
 			
-			// VARIABLE DECLARATION
-			//---------------------------------
-			$obj = $(this);			
-			var rumbleInterval;	
-			var rangeX = opt.rangeX;
-			var rangeY = opt.rangeY;
-			var rangeRot = opt.rangeRot;
-			rangeX = rangeX*2;
-			rangeY = rangeY*2;
-			rangeRot = rangeRot*2;
-			var rumbleSpeed = opt.rumbleSpeed;			
-			var objPosition = $obj.css('position');
-			var objXrel = opt.posX;
-			var objYrel = opt.posY;
-			var objXmove;
-			var objYmove;
-			var inlineChange;
-			
-			// SET POSITION RELATION IF CHANGED
-			//---------------------------------
-			if(objXrel === 'left'){
-				objXmove = parseInt($obj.css('left'),10);
-			}
-			if(objXrel === 'right'){
-				objXmove = parseInt($obj.css('right'),10);
-			}
-			if(objYrel === 'top'){
-				objYmove = parseInt($obj.css('top'),10);
-			}
-			if(objYrel === 'bottom'){
-				objYmove = parseInt($obj.css('bottom'),10);
-			}
-			
-			// RUMBLER FUNCTION
-			//---------------------------------			
-			function rumbler(elem) {				
-				var randBool = Math.random();
-				var randX = Math.floor(Math.random() * (rangeX+1)) -rangeX/2;
-				var randY = Math.floor(Math.random() * (rangeY+1)) -rangeY/2;
-				var randRot = Math.floor(Math.random() * (rangeRot+1)) -rangeRot/2;	
+			/*========================================================*/
+			/* Rumble Function
+			/*========================================================*/		
+			var rumbler = function(){				
+				var rx = Math.floor(Math.random() * (x+1)) -x/2,
+					ry = Math.floor(Math.random() * (y+1)) -y/2,
+					rrot = Math.floor(Math.random() * (rot+1)) -rot/2,
+					ropac = opac ? Math.random() + opacm : 1;
+					
+				/*========================================================*/
+				/* Ensure Movement From Original Position
+				/*========================================================*/				
+				rx = (rx === 0 && x !== 0) ? ((Math.random() < .5) ? 1 : -1) : rx;
+				ry = (ry === 0 && y !== 0) ? ((Math.random() < .5) ? 1 : -1) : ry;	
 				
-				// IF INLINE, MAKE INLINE-BLOCK FOR ROTATION
-				//---------------------------------
-				if(elem.css('display') === 'inline'){
-					inlineChange = true;
-					elem.css('display', 'inline-block')
-				}
-			
-				// ENSURE MOVEMENT
-				//---------------------------------			
-				if(randX === 0 && rangeX !== 0){
-					if(randBool < .5){
-						randX = 1;
-					}
-					else {
-						randX = -1;
-					}
+				/*========================================================*/
+				/* Check Inline
+				/*========================================================*/
+				if($this.css('display') === 'inline'){
+					inline = true;
+					$this.css('display', 'inline-block');
 				}
 				
-				if(randY === 0 && rangeY !== 0){
-					if(randBool < .5){
-						randY = 1;
-					}
-					else {
-						randY = -1;
-					}
-				}
-				
-				// RUMBLE BASED ON POSITION
-				//---------------------------------
-				if(objPosition === 'absolute'){
-					elem.css({'position':'absolute','-webkit-transform': 'rotate('+randRot+'deg)', '-moz-transform': 'rotate('+randRot+'deg)', '-o-transform': 'rotate('+randRot+'deg)', 'transform': 'rotate('+randRot+'deg)'});
-					elem.css(objXrel, objXmove+randX+'px');
-					elem.css(objYrel, objYmove+randY+'px');
-				}
-				if(objPosition === 'fixed'){
-					elem.css({'position':'fixed','-webkit-transform': 'rotate('+randRot+'deg)', '-moz-transform': 'rotate('+randRot+'deg)', '-o-transform': 'rotate('+randRot+'deg)', 'transform': 'rotate('+randRot+'deg)'});
-					elem.css(objXrel, objXmove+randX+'px');
-					elem.css(objYrel, objYmove+randY+'px');
-				}
-				if(objPosition === 'static' || objPosition === 'relative'){
-					elem.css({'position':'relative','-webkit-transform': 'rotate('+randRot+'deg)', '-moz-transform': 'rotate('+randRot+'deg)', '-o-transform': 'rotate('+randRot+'deg)', 'transform': 'rotate('+randRot+'deg)'});
-					elem.css(objXrel, randX+'px');
-					elem.css(objYrel, randY+'px');
-				}
-			} // End rumbler function
-			
-			// EVENT TYPES (rumbleEvent)
-			//---------------------------------	
-			var resetRumblerCSS = {'position':objPosition,'-webkit-transform': 'rotate(0deg)', '-moz-transform': 'rotate(0deg)', '-o-transform': 'rotate(0deg)', 'transform': 'rotate(0deg)'};
-			
-			if(opt.rumbleEvent === 'hover'){
-				$obj.hover(
-					function() {
-						var rumblee = $(this);
-						rumbleInterval = setInterval(function() { rumbler(rumblee); }, rumbleSpeed);
-					},
-					function() {
-						var rumblee = $(this);
-						clearInterval(rumbleInterval);
-						rumblee.css(resetRumblerCSS);
-						rumblee.css(objXrel, objXmove+'px');
-						rumblee.css(objYrel, objYmove+'px');
-						if(inlineChange === true){
-							rumblee.css('display','inline');
-						}
-					}
-				);
-			}
-			
-			if(opt.rumbleEvent === 'click'){
-				$obj.toggle(function(){
-					var rumblee = $(this);
-					rumbleInterval = setInterval(function() { rumbler(rumblee); }, rumbleSpeed);
-				}, function(){
-					var rumblee = $(this);
-					clearInterval(rumbleInterval);
-					rumblee.css(resetRumblerCSS);
-					rumblee.css(objXrel, objXmove+'px');
-					rumblee.css(objYrel, objYmove+'px');
-					if(inlineChange === true){
-						rumblee.css('display','inline');
-					}
+				/*========================================================*/
+				/* Rumble Element
+				/*========================================================*/			
+				$this.css({
+					'position':'relative',
+					'left':rx+'px',
+					'top':ry+'px',
+					'-ms-filter':'progid:DXImageTransform.Microsoft.Alpha(Opacity='+ropac*100+')',
+					'filter':'alpha(opacity='+ropac*100+')',					
+					'-moz-opacity':ropac,					
+					'-khtml-opacity':ropac,					
+					'opacity':ropac,
+					'-webkit-transform':'rotate('+rrot+'deg)', 
+					'-moz-transform':'rotate('+rrot+'deg)', 
+					'-ms-transform':'rotate('+rrot+'deg)',
+					'-o-transform':'rotate('+rrot+'deg)', 
+					'transform':'rotate('+rrot+'deg)'
 				});
-			}
+			};
 			
-			if(opt.rumbleEvent === 'mousedown'){
-				$obj.bind({
-					mousedown: function(){
-						var rumblee = $(this);
-						rumbleInterval = setInterval(function() { rumbler(rumblee); }, rumbleSpeed);
-					}, 
-					mouseup: function(){
-						var rumblee = $(this);
-						clearInterval(rumbleInterval);
-						rumblee.css(resetRumblerCSS);
-						rumblee.css(objXrel, objXmove+'px');
-						rumblee.css(objYrel, objYmove+'px');
-						if(inlineChange === true){
-							rumblee.css('display','inline');
-						}
-					},
-					mouseout: function(){
-						var rumblee = $(this);
-						clearInterval(rumbleInterval);
-						rumblee.css(resetRumblerCSS);
-						rumblee.css(objXrel, objXmove+'px');
-						rumblee.css(objYrel, objYmove+'px');
-						if(inlineChange === true){
-							rumblee.css('display','inline');
-						}
+			/*========================================================*/
+			/* Rumble CSS Reset
+			/*========================================================*/
+			var reset = {
+				'left':0,
+				'top':0,
+				'-ms-filter':'progid:DXImageTransform.Microsoft.Alpha(Opacity=100)',
+				'filter':'alpha(opacity=100)',					
+				'-moz-opacity':1,					
+				'-khtml-opacity':1,					
+				'opacity':1,
+				'-webkit-transform':'rotate(0deg)',
+				'-moz-transform':'rotate(0deg)',
+				'-ms-transform':'rotate(0deg)',
+				'-o-transform':'rotate(0deg)',
+				'transform':'rotate(0deg)'
+			};
+			
+			/*========================================================*/
+			/* Rumble Start/Stop Trigger
+			/*========================================================*/
+			$this.bind({
+				'startRumble': function(e){
+					e.stopPropagation();
+					clearInterval(interval);
+					interval = setInterval(rumbler, speed)
+				},
+				'stopRumble': function(e){
+					e.stopPropagation();
+					clearInterval(interval);
+					if(inline){
+						$this.css('display', 'inline');
 					}
-				});
-			}
+					$this.css(reset);
+				}
+			});		
 			
-			if(opt.rumbleEvent === 'constant'){
-				var rumblee = $(this);
-				rumbleInterval = setInterval(function() { rumbler(rumblee); }, rumbleSpeed);
-			}
-			
-		});
-	}; 
+		});// End return this.each
+	};// End $.fn.jrumble
 })(jQuery);
--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
-jRumble v1.2 - http://jackrugile.com/jrumble
+jRumble v1.3 - http://jackrugile.com/jrumble
 by Jack Rugile - http://jackrugile.com
 Copyright 2011, Jack Rugile
 MIT license - http://www.opensource.org/licenses/mit-license.php
